76ce9e60f90f1603b7a7a802942955cbdc4ce737
shubhangitayade2 <105914219+shubhangitayade2@users.noreply.github.com>
2022-06-20 11:17:48 +0530

    home code

---
+++ home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import { CustomerComponent } from "../home/HomeComponent.";
+
+
+
+@Component({
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
